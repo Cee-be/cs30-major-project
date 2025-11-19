a73937496e34546fa93d54ae4a2d12f7ffa9de25
Cee-be <7015733@spsd.sk.ca>
2025-11-19 09:53:49 -0600

    files of songs to include and load

--- sketch.js
+++ sketch.js
@@ -1,13 +1,15 @@
-// Project Title
-// Your Name
+// Major project - Pitch Perfect
+// Ceberta Adum
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let song;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  song = loadSound('Omah-Lay-Holy-Ghost.mp3');
 }
 
 function draw() {
