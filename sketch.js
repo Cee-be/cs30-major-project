a5739d28221961bf5400c31bee856feabaad1acc
Cee-be <7015733@spsd.sk.ca>
2025-11-23 22:43:46 -0600

    including, reset and pause button

--- sketch.js
+++ sketch.js
@@ -25,9 +25,11 @@ let lyrics = [];
 let yStart = 0;
 let time;
 let analyzer;
-let btn;
+let startBtn;
+let pauseBtn;
+let resetBtn;
 let started = false;
-let btnvisi = true;
+//let btnvisi = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -38,12 +40,31 @@ function setup() {
 }
 
 function buttonfunction(){
-  btn = createButton("Start Karaoke");
-  btn.style('width', '750px');
-  btn.style('height', '80px');
-  btn.style('font-size', '30px');
-  btn.position(width/2 - 350, height/2 - 100);
-  btn.mousePressed(startKaraoke);
+  //Start Button
+  startBtn = createButton("Start Karaoke");
+  startBtn.style('width', '750px');
+  startBtn.style('height', '80px');
+  startBtn.style('font-size', '30px');
+  startBtn.position(width/2 - 350, height/2 - 100);
+  startBtn.mousePressed(startKaraoke);
+
+  //Pause Button
+  pauseBtn = createButton("Pause");
+  pauseBtn.style('width', '150px');
+  pauseBtn.style('height', '50px');
+  pauseBtn.style('font-size', '30px');
+  pauseBtn.position(50, 100);
+  pauseBtn.mousePressed(pauseKaraoke);
+  pauseBtn.hide();
+
+  //Reset Button
+  resetBtn = createButton("Reset");
+  resetBtn.style('width', '150x');
+  resetBtn.style('height', '50px');
+  resetBtn.style('font-size', '30px');
+  resetBtn.position(20, 100);
+  resetBtn.mousePressed(resetKaraoke);
+  resetBtn.hide();
 }
 
 function preload(){
@@ -69,12 +90,12 @@ function start(){
     text("Click 'Start Karaoke' to begin", width/2 - 200 , height/2 + 20);
   }
 
-  if (btnvisi){
-    btn.show();
-  }
-  else {
-    btn.hide();
-  }
+  // if (btnvisi){
+  //   btn.show();
+  // }
+  // else {
+  //   btn.hide();
+  // }
 }
 
 function mousePressed(){
@@ -89,6 +110,31 @@ function startKaraoke(){
   analyzer.setInput(song);
   song.loop();
   started = true;
+
+  startBtn.hide();
+  pauseBtn.show();
+  resetBtn.show();
+}
+
+function pauseKaraoke(){
+  if (song.isPlaying()){
+    song.pause();
+  }
+  else {
+    song.play();
+  }
+}
+
+function resetKaraoke(){
+  song.stop();
+  song.currentTime(0);
+
+  started = false;
+  btnvisi = true;
+
+  startBtn.show();
+  pauseBtn.hide();
+  resetBtn.hide();
 }
 
 function lyricDisplay(){
