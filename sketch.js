5ef527c7df312dbc4f3b23e3bc672f3efdf56faa
Cee-be <7015733@spsd.sk.ca>
2025-12-03 00:53:48 -0600

    using classs=

--- sketch.js
+++ sketch.js
@@ -19,9 +19,8 @@
 //   }
 // ];
 
-let song;
+let currentSong;
 let rawLyrics;
-let lyrics = [];
 let yStart = 0;
 let time;
 let currentButton;
@@ -35,28 +34,77 @@ let btnvisi = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rectMode(CENTER);
+  rectMode(CORNER);
 
-  parseLyric();
+  karaoke = new KaraokeLyrics();
+  karaoke.load(rawLyrics);
   buttonfunction();
+}
+
+function preload(){
+  currentSong = loadSound('justin-bieber_baby.mp3');
+  rawLyrics = loadStrings("baby_lyrics.txt");
+  play = loadImage("play-button.png");
+  pause = loadImage("pause-button.png");
+  reset = loadImage("reset.png");
+
+  currentButton = loadImage("pause-button.png");
 }
 
 class KaraokeLyrics {
   constructor(){
+    this.lyrics = [];
+  }
 
+  load(rawLyrics){
+    for (let line of rawLyrics){
+      this.parts = line.split("|");
+
+      this.lyrics.push({
+        timeStart: Number(this.parts[0]),
+        timeStop: Number(this.parts[1]),
+        lyric: this.parts[2]
+      });
+    }
+  }
+
+  findCurrentLyric(currentTime){
+    for (let entry of this.lyrics){
+      if (currentTime >= entry.timeStart && currentTime <= entry.timeStop){
+        return entry;
+      }
+    }
+  }
+
+  //displaying the lyrics
+  display(currentTime){
+    this.lyric = this.findCurrentLyric(currentTime);
+    if (!this.lyric) {
+      return;
+    }
+    this.progress = (currentTime - this.lyric.timeStart) / 
+                    (this.lyric.timeStop - this.lyric.timeStart);
+    this.w = textWidth(this.lyric.lyric); 
+    this.highlightWidth = this.w * this.progress;
+
+    //design for the bar
+    textSize(36);
+    let h = 36 *1.2;
+    let cx = width/2 - this.w/2; //22closer to words
+    //let cl = cx - w/2; //start from left
+    let cy = height/2 - h/2;//-20 tomove up
+    fill('black');
+    rect(cx + 22, cy - 20, abs(this.highlightWidth), h); //hW might be neg
+
+    //lyric text
+    textAlign(LEFT, CENTER);
+    fill(255);
+    stroke(0);
+    strokeWeight(3);
+    text(this.lyric.lyric, cx, cy);
   }
 }
 
-function parseLyric(){
-  for (let line of rawLyrics){
-    let parts = line.split("|");
-    lyrics.push({
-      timeStart: Number(parts[0]),
-      timeStop: Number(parts[1]),
-      lyric: parts[2]
-    });
-  }
-}
 
 function buttonfunction(){
 
@@ -93,25 +141,15 @@ function buttonfunction(){
   resetBtn.hide();
 }
 
-function preload(){
-  song = loadSound('justin-bieber_baby.mp3');
-  rawLyrics = loadStrings("baby_lyrics.txt");
-  play = loadImage("play-button.png");
-  pause = loadImage("pause-button.png");
-  reset = loadImage("reset.png");
-
-  currentButton = loadImage("pause-button.png");
-}
-
 function draw() {
   background(220);
-  //lyricDisplay();
   start();
 }
 
 function start(){
+  //button press start
   if (started){
-    lyricDisplay();
+    karaoke.display(currentSong.currentTime());
   }
   else{
     textFont('Courier New');
@@ -123,6 +161,7 @@ function start(){
     text("Click 'Start Karaoke' to begin", width/2 , height/2);
   }
 
+  //hide and show of Srtbtn
   if (btnvisi){
     btn.show();
   }
@@ -131,19 +170,24 @@ function start(){
   }
 }
 
+// What happens when msc srts
 function startKaraoke(){
   analyzer = new p5.Amplitude(0, 5);
-  analyzer.setInput(song);
-  song.loop();
+  analyzer.setInput(currentSong);
+  currentSong.loop();
   started = true;
+
+  btnvisi = false;
+
   pauseBtn.show();
   playBtn.hide();
   resetBtn.show();
 }
 
+//reset button actio
 function resetKaraoke(){
-  song.stop();
-  song.jump(0);
+  currentSong.stop();
+  currentSong.jump(0);
   started = false;
   btnvisi = true;
 
@@ -152,42 +196,17 @@ function resetKaraoke(){
   resetBtn.hide();
 }
 
+//when pause is pressed
 function togglePause(){
-  if (song.isPlaying()){
-    song.pause();
+  if (currentSong.isPlaying()){
+    currentSong.pause();
     pauseBtn.hide();
     playBtn.show();
   }
   else{
-    song.play();
+    currentSong.play();
     pauseBtn.show();
     playBtn.hide();
   }
 }
 
-function lyricDisplay(){
-  time = song.currentTime();
-
-  for (let entry of lyrics){
-    if (time >= entry.timeStart && time <= entry.timeStop){
-      progress = (time - entry.timeStart) / (entry.timeStart - entry.timeStop);
-      w = textWidth(entry.lyric); //define at top
-      highlightWidth = w * progress;
-      
-      textSize(36);
-      cx = width/2;
-      cy = height/2;
-      h = 36 *1.2;
-      fill('black');
-      rect(cx, cy, highlightWidth, h);
-
-      textAlign(CENTER, CENTER);
-      fill(255);
-      stroke(0);
-      strokeWeight(3);
-      text(entry.lyric, cx, cy);
-
-      break;
-    }
-  }
-}
